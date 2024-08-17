d68e87429c058e962074cc309c7d309834e2d8e4
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-08-17 21:21:04 +0300

    init

---
+++ migrations/m240111_000111_create_news_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+class m240111_000111_create_news_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('news', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'content' => $this->text()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('news');
+    }
+}
