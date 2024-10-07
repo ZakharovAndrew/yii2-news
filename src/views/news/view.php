085cdb6f558b9fc3e5b214cd59d66e1eeff22dd5
Zakharov Andrey <37038505+ZakharovAndrew@users.noreply.github.com>
2024-10-08 01:23:08 +0300

    Add files via upload

--- src/views/news/view.php
+++ src/views/news/view.php
@@ -4,6 +4,10 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use ZakharovAndrew\news\Module;
 
+use ZakharovAndrew\user\assets\UserAssets;
+use yii\helpers\Url;
+UserAssets::register($this);
+
 /* @var $this yii\web\View */
 /* @var $model ZakharovAndrew\news\models\News */
 
@@ -24,18 +28,45 @@ $this->params['breadcrumbs'][] = $this->title;
         margin-right: 16px;
         cursor: pointer;
         white-space: nowrap;
+        text-decoration: none;
     }
     .comment-child {
-        border: 1px solid #ebebeb;
         border-radius: 12px;
-        margin-left: 10px; 
+        margin-left: 26px;
+        padding: 10px;
+        position: relative
+    }
+    .comment .comment-child::before {
+        content: "";
+        display: block;
+        border-left: 1px solid #e5e5e5;
+        border-bottom: 1px solid #e5e5e5;
+        border-radius: 0 0 0 10px;
+        position: absolute;
+        width: 16px;
+        height: 31px;
+        margin-left: -25px;
+        margin-top: -10px;
+    }
+    .comment .comment-child::after {
+        content: "";
+        display: block;
+        border-left: 1px solid #e5e5e5;
+        height: 100%;
+        width: 26px;
+        margin-left: -25px;
+        top: 0;
+        position: absolute;
+    }
+    .comment .comment-child:last-child::after {
+        display: none;
     }
     .comment-form textarea {
         background: #ebebeb
     }
     .comment-form .help-block {display:none !important;}
     .comment-head {display:flex;gap:10px;margin-bottom:5px;}
-    .comment-avatar{width:42px;height:42px;border-radius:50%;background:#ebebeb}
+    .comment-avatar, .comment-avatar img{width:42px;height:42px;border-radius:50%;background:#ebebeb}
     .comment-author {
         font-size: 15px;
         line-height: 22px;
@@ -49,6 +80,32 @@ $this->params['breadcrumbs'][] = $this->title;
     .comment-datetime {
         font-size:13px;
         color:#595959;
+    }
+    .comment-block {
+        background: #fff;
+        border-radius: 12px;
+        padding:20px;
+        margin-top:20px;
+    }
+    .comment-actions {
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+        margin: 7px 0;
+    }
+    .comment-actions a {
+        margin-right: 16px;
+        font-size:14px;
+        text-decoration: none;
+    }
+    .comment-actions a:hover {
+        text-decoration: underline;
+    }
+    .comment-content {
+        font-size:17px;
+    }
+    body {
+        background: #ebebeb;
     }
 </style>
 
@@ -68,40 +125,45 @@ $this->params['breadcrumbs'][] = $this->title;
 $comments = $model->getComments()->where('parent_id is null')->orderBy('created_at DESC')->all();
 $cnt_comment = $model->getComments()->count();
 ?>
-<div class="comments-count">Комментарии (<?=$cnt_comment?>):</div>
+<div class="comment-block">
+    <div class="comments-count">Комментарии (<?=$cnt_comment?>):</div>
 
-<div class="comment-form">
+    <div class="comment-form">
 
-    <?php $form = ActiveForm::begin(['action' => ['comment/create', 'news_id' => $model->id]]);?>
+        <?php $form = ActiveForm::begin(['action' => ['comment/create', 'news_id' => $model->id]]);?>
 
-    <?= $form->field($modelComment, 'content')->textarea(['rows' => 3, 'placeholder' => "Комментарий..."])->label(false)?>
+        <?= $form->field($modelComment, 'content')->textarea(['rows' => 3, 'placeholder' => "Комментарий..."])->label(false)?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Добавить комментарий', ['class' => 'btn btn-primary'])?>
-    </div>
-
-    <?php ActiveForm::end();?>
-
-</div>
-
-<?php foreach ($comments as $comment):?>
-    <div class="comment">
-        <div class="comment-head">
-            <div class="comment-avatar"></div>
-            <div class="comment-author"><?= $comment->author->username?><div class="comment-datetime"><?= date('d.m.Y H:i:s', strtotime($comment->created_at)) ?></div></div>
+        <div class="form-group">
+            <?= Html::submitButton('Добавить комментарий', ['class' => 'btn btn-primary'])?>
         </div>
-        <p><?= $comment->content?></p>
-        <p><?= Html::a('Ответить', ['comment/create', 'news_id' => $model->id, 'parent_id' => $comment->id], ['class' => 'news-btn-comment'])?> | <?= Html::a('Редактировать', ['comment/update', 'id' => $comment->id])?> | <?= Html::a('Удалить', ['comment/delete', 'id' => $comment->id])?></p>
-        <?php foreach ($comment->children as $child):?>
-            <div class="comment-child">
-                <div class="comment-head">
-                    <div class="comment-avatar"></div>
-                    <div class="comment-author"><?= $child->author->username?><div class="comment-datetime"><?= date('d.m.Y H:i:s', strtotime($child->created_at)) ?></div></div>
-                </div>
-                <p><?= $child->content?></p>
-                <p><?= Html::a('Редактировать', ['comment/update', 'id' => $child->id])?> | <?= Html::a('Удалить', ['comment/delete', 'id' => $child->id])?></p>
-            </div>
-        <?php endforeach;?>
-        
+
+        <?php ActiveForm::end();?>
+
     </div>
-<?php endforeach;?>
+
+    <?php foreach ($comments as $comment):?>
+        <div class="comment">
+            <div class="comment-head">
+                <div class="comment-avatar"><img src="<?= !$comment->author->getAvatarUrl() ?
+                                Yii::$app->assetManager->getAssetUrl(UserAssets::register($this), 'images/default-avatar.png') :
+                                $comment->author->getAvatarUrl()
+                            ?>" alt="Avatar"></div>
+                <div class="comment-author"><?= $comment->author->username?><div class="comment-datetime"><?= date('d.m.Y H:i:s', strtotime($comment->created_at)) ?></div></div>
+            </div>
+            <div><?= $comment->content?></div>
+            <div class="comment-actions"><?= Html::a('Ответить', ['comment/create', 'news_id' => $model->id, 'parent_id' => $comment->id], ['class' => 'news-btn-comment'])?> <?= Html::a('Редактировать', ['comment/update', 'id' => $comment->id])?> <?= Html::a('Удалить', ['comment/delete', 'id' => $comment->id])?></div>
+            <?php foreach ($comment->children as $child):?>
+                <div class="comment-child">
+                    <div class="comment-head">
+                        <div class="comment-avatar"></div>
+                        <div class="comment-author"><?= $child->author->username?><div class="comment-datetime"><?= date('d.m.Y H:i:s', strtotime($child->created_at)) ?></div></div>
+                    </div>
+                    <div class="comment-content"><?= $child->content?></div>
+                    <div class="comment-actions"><?= Html::a('Редактировать', ['comment/update', 'id' => $child->id])?> <?= Html::a('Удалить', ['comment/delete', 'id' => $child->id])?></div>
+                </div>
+            <?php endforeach;?>
+
+        </div>
+    <?php endforeach;?>
+</div>
