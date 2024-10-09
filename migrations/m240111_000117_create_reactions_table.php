f71492ed19e49bf6b6f1e2eef21f2f739ca0e45b
Zakharov Andrey <prostonado_07@mail.ru>
2024-10-09 21:34:33 +0300

    fixed

--- migrations/m240111_000117_create_reactions_table.php
+++ migrations/m240111_000117_create_reactions_table.php
@@ -8,13 +8,14 @@ class m240111_000117_create_reactions_table extends Migration
     {
         $this->createTable('{{%reactions}}', [
             'id' => $this->primaryKey(),
-            'name' => $this->string()->notNull(),
+            'name' => $this->string(255)->notNull(),
+            'css_class' => $this->string(255)->notNull()
         ]);
 
-        $this->insert('{{%reactions}}', ['id' => 1, 'name' => 'Палец вверх']);
-        $this->insert('{{%reactions}}', ['id' => 2, 'name' => 'Палец вниз']);
-        $this->insert('{{%reactions}}', ['id' => 3, 'name' => 'Огонь']);
-        $this->insert('{{%reactions}}', ['id' => 4, 'name' => 'Слеза']);
+        $this->insert('{{%reactions}}', ['id' => 1, 'name' => 'Like', 'css_class' => 'reaction_like']);
+        $this->insert('{{%reactions}}', ['id' => 2, 'name' => 'Fire', 'css_class' => 'reaction_fire']);
+        $this->insert('{{%reactions}}', ['id' => 3, 'name' => 'Dislike', 'css_class' => 'reaction_dislike']);
+        $this->insert('{{%reactions}}', ['id' => 4, 'name' => 'Laught', 'css_class' => 'reaction_laught']);
     }
 
     public function down()
