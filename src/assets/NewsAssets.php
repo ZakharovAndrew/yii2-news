de4fea6c8e5ac85e675aad834c16bc79c052c717
Zakharov Andrey <prostonado_07@mail.ru>
2024-10-09 21:35:22 +0300

    added assets

---
+++ src/assets/NewsAssets.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://github.com/ZakharovAndrew/yii2-user
+ * @copyright Copyright (c) 2024 Zakharov Andrey
+ */
+
+namespace ZakharovAndrew\news\assets;
+
+use yii\web\AssetBundle;
+
+class NewsAssets extends AssetBundle
+{
+    public $sourcePath = '@vendor/zakharov-andrew/yii2-news/src/assets';
+
+    public $css = [
+        'css/style.css',
+    ];
+
+    public $js = [
+    //    'js/script.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        //'yii\bootstrap5\BootstrapAsset',
+    ];
+}
